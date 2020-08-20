5ece428de0dae8a243c5571f9ee4ff2fd74e8e1a
Breno Souza Costa <32497952+brenoSCosta@users.noreply.github.com>
2020-08-20 18:33:31 -0300

    Mod admin

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status')
     search_fields = ('title', 'content')
     date_hierarchy = 'published'
+    raw_id_fields = ('author',)
     list_filter = ('status', 'created', 'published', 'author')
     prepopulated_fields = {'slug': ('title',)}
