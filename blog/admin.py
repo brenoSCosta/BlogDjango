e462e359741837cc787d0b71415b89a81bd60136
Breno Souza Costa <32497952+brenoSCosta@users.noreply.github.com>
2020-08-20 17:34:12 -0300

    Mod admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status')
     search_fields = ('title', 'content')
+    date_hierarchy = 'published'
+    list_filter = ('status', 'created', 'published', 'author')
+    prepopulated_fields = {'slug': ('title',)}
